190232aee81c4177760f9d16ebf8ef4ec2805c66
Mark Delgado <markdelgado.md@hotmail.com>
2022-08-10 19:30:02 -0400

    commit tree f79382228a2ef7dc24aa4d7acc1bd7f966260304

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,13 @@
 class Dog
+    # attr_writer :name
+    attr_accessor :name, :breed
+    def name=(name)
+        @name= name
+    end
+
+    def breed=(breed)
+        @breed= breed
+    end
+    
 
 end
